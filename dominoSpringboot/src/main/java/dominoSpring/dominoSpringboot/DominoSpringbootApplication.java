6c22b76058c05194194992b41feb331e51d38897
Domino419 <noris419@naver.com>
2023-08-14 23:49:40 +0900

    섹션3_독립실행형 서블릿 애플리케이션 - Containerless 개발 준비

--- dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/DominoSpringbootApplication.java
+++ dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/DominoSpringbootApplication.java
@@ -1,5 +1,19 @@
 package dominoSpring.dominoSpringboot;
 
+
+public class DominoSpringbootApplication {
+	public static void main(String[] args) {
+		System.out.println("Run 되는지 확인용 ");
+
+	}
+}
+
+
+
+/*
+ 스프링부트가 없다는 걸 가정하고 코드를 작성해보자.
+ Containerless 개발 준비
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +24,6 @@ public class DominoSpringbootApplication {
 		SpringApplication.run(DominoSpringbootApplication.class, args);
 	}
 
+
 }
+*/
--- dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/HelloController.java
+++ dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/HelloController.java
@@ -6,9 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping ("/Hello")
-    //
+    //http://localhost:8080/Hello?name=domino
     public String Hello(String name) {
-        return "Hello" + name ;
+        if(name != null) {
+            System.out.println("나와라 얍얍") ;
+            return "Hello  " + name;
+        }
+        else {
+            System.out.println("나와라 얍얍") ;
+            return "Hello  " + "익명";
+        }
     }
+
 
 }
