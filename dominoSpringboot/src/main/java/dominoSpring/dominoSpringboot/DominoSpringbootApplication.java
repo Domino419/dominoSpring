fbc1a2b6adfd03c14e15e79fb46b10b8a2975cd2
Domino419 <noris419@naver.com>
2023-08-15 00:35:28 +0900

    섹션3_독립실행형 서블릿 애플리케이션 - 서블릿 등록

--- dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/DominoSpringbootApplication.java
+++ dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/DominoSpringbootApplication.java
@@ -1,9 +1,42 @@
 package dominoSpring.dominoSpringboot;
 
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class DominoSpringbootApplication {
 	public static void main(String[] args) {
-		System.out.println("Run 되는지 확인용 ");
+		System.out.println("서블릿 컨테이너 띄우기 실습 시작 ");
+		TomcatServletWebServerFactory serverfactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverfactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
+		webServer.start();
+
+		System.out.println("서블릿 컨테이너 떳다  !  ");
+
+
+
+
+
 
 	}
 }
