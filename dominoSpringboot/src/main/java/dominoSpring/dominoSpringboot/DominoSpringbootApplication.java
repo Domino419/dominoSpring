fbeb618d29db8da51698c52f34a80546c290fc97
Domino419 <noris419@naver.com>
2023-08-20 22:50:36 +0900

    섹션4_독립실행형 스프링 애플리케이션 - 스프링 컨테이너로 통합

--- dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/DominoSpringbootApplication.java
+++ dominoSpringboot/src/main/java/dominoSpring/dominoSpringboot/DominoSpringbootApplication.java
@@ -1,39 +1,36 @@
 package dominoSpring.dominoSpringboot;
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@SpringBootApplication
+// @SpringBootApplication
 public class DominoSpringbootApplication {
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너를 대표하는 인터페이스  ApplicationContext 에 클래스를 직접 등록
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+									new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+				webServer.start();
+
+			}
+		} ;
 		applicationContext.registerBean(HelloController.class) ;
 		applicationContext.registerBean(SimpleHelloService.class) ;
-		applicationContext.refresh();   // <- 등록하고 나서 초기화 처리
+		applicationContext.refresh();  //<< 스프링 컨테이너의 초기화 작업
 
-		TomcatServletWebServerFactory serverfactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverfactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-		});
-		webServer.start();
 		System.out.println("서버 기동 완료!!  ");
 	}
 }
